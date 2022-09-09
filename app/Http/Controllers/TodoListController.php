cb84ba806a692c0dad4332bbc27dfcd83ec6c68a
LenSys <[omitted]>
2022-09-09 10:06:51 +0200

    added save to database functionality for new list items in saveItem() method for TodoListController

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ListItem;
 
 class TodoListController extends Controller
 {
     //
     public function saveItem(Request $request) {
-      \Log::info(json_encode($request->all()));
+
+      // check if list item has been filled out in form
+      if($request->listItem !== null) {
+        // save new list item in database table
+        $listItem = new ListItem();
+        $listItem->name = $request->listItem;
+        $listItem->is_complete = 0;
+        $listItem->save();
+      }
+
       return redirect()->route('root')->with('success', 'Item successfully created!');
     }
 }
